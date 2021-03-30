f46acbc4facaa1543e97fe0e427bbd909b0793a0
Uzan Latif <69574011+uzanlatif@users.noreply.github.com>
2021-03-30 15:58:26 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,16 @@
+import java.util.*;  
+import java.util.Scanner; 
+public class Mahasiswa{
+
+	String Name;
+	Integer Nim,Usia,Uts,Uas;
+
+	//iki namanya constructor
+	public Mahasiswa(String x,Integer m,Integer y,Integer z,Integer b){
+		this.Name = x;
+		this.Nim = m;
+		this.Usia = y;
+		this.Uts = z;
+		this.Uas = b;
+	}
+}
